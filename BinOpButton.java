4a43d813e7117beeed29bacaa4eff82d26294268
Khaled <75619589+Hohki@users.noreply.github.com>
2023-02-28 22:11:18 +0100

    Implemention av CalculatorButton + började på BinOpButton
    Implemention av CalculatorButton + började på BinOpButton

--- BinOpButton.java
+++ BinOpButton.java
@@ -1,5 +1,27 @@
 package Lab5;
 
-public class BinOpButton {
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.util.function.IntBinaryOperator;
 
+public class BinOpButton extends CalculatorButton {
+    private IntBinaryOperator intBinaryOperator;
+
+    public BinOpButton(String str, Situation situation, ActionListener listener, IntBinaryOperator intBinaryOperator) {
+        super(str, situation, listener);
+        this.intBinaryOperator = intBinaryOperator;
+    }
+
+    //?????????????
+    @Override
+    public void transition(int x, int y) {
+        if (situation.state == State.Input1 || situation.state == State.HasResult) {
+            situation.leftOperand = x;
+            situation.binaryOperator = this;
+            this.setColor(Color.RED);
+        } else if (situation.state == State.OpReady) {
+            this.setColor(Color.WHITE);
+            situation.binaryOperator = this;
+        }
+    }
 }
